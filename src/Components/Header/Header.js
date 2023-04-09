7ea5267c333790b44abe475365419a9973346579
masood911 <masoodjutt64@gmail.com>
2023-04-08 23:58:50 -0700

    Today work 4-8-23 / 12:00

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -96,6 +96,13 @@ export default function Header() {
                     Appointment
                   </Link>
                 </li>
+                <li className="nav-item">
+                  <Link to="/find-donor" className="nav-link">
+                    <div className="circle">
+                      <span className="circle-inner" data-hover="Profile">SB</span>
+                    </div>
+                  </Link>
+                </li>
               </ul>
             </div>
             <div className="navbar-out order-2 order-xl-3">
