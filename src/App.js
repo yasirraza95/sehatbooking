981efab5cbdfd1981f60bd1bdce6352d6687f22b
Yasir Raza <raza.yasir95@gmail.com>
2023-04-11 21:05:33 +0500

    whatsapp floating button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+
 import "./App.css";
 import AboutUs from "./Components/AboutUs/AboutUs";
 import Campaign from "./Components/Campaign/Campaign";
@@ -18,28 +20,42 @@ import HowTo from "./Components/How to/HowTo";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<IndexOne />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/index-one" element={<IndexOne />} />
-          {/* <Route path="/faqs" element={<Faqs />} /> */}
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-conditions" element={<TermsConditions />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/forgot-pass" element={<ForgotPass />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-          {/* <Route path="/campaign" element={<Campaign />} /> */}
-          <Route path="/blood-tips" element={<BloodTips />} />
-          <Route path="/find-donor" element={<FindDonor />} />
-          <Route path="/appointment" element={<Appointment />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/how-to" element={<HowTo />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <div className="App">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<IndexOne />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/index-one" element={<IndexOne />} />
+            {/* <Route path="/faqs" element={<Faqs />} /> */}
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-conditions" element={<TermsConditions />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/forgot-pass" element={<ForgotPass />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            {/* <Route path="/campaign" element={<Campaign />} /> */}
+            <Route path="/blood-tips" element={<BloodTips />} />
+            <Route path="/find-donor" element={<FindDonor />} />
+            <Route path="/appointment" element={<Appointment />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/how-to" element={<HowTo />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <FloatingWhatsApp
+        phoneNumber="+923137300434"
+        accountName="Sehat Booking"
+        chatMessage="Assalam o Alaikum, Main apki kya madad kr skta hu?"
+        statusMessage=""
+        placeholder="Type a message"
+        avatar="/favicon.png"
+        allowEsc
+        allowClickAway
+        notification
+        notificationSound
+      />
+    </>
   );
 }
 
