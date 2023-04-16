6c2533ba6afb8293b6761c4fab225fc68cb7119c
masood911 <masoodjutt64@gmail.com>
2023-04-16 01:20:27 -0700

    How to page Done

--- src/Components/How to/HowTo.js
+++ src/Components/How to/HowTo.js
@@ -4,7 +4,9 @@ import Header from "../Header/Header";
 // import ToTop from "../TopTop/ToTop";
 import Footer from "../Footer/Footer";
 import useImportScript from "../../utils/useImportScript";
-
+import "./HowTo.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHospital } from "@fortawesome/free-solid-svg-icons";
 export default function HowTo() {
   useImportScript("/assets/vendor/jquery/jquery-3.6.0.min.js");
   useImportScript("/assets/vendor/bootstrap/js/bootstrap.bundle.min.js");
@@ -60,96 +62,260 @@ export default function HowTo() {
             <div className="col-lg-12">
               <div className="contact-area">
                 <div className="contact-area__content">
+                  {/* ____________________ */}
+
+                  {/* ____________________ */}
+
+
+                  {/* <!-- Login / Register Modal--> */}
                   <div className="row neutral-row">
+
                     <div className="col-lg-5 row-item">
                       <div className="contact-area-single contact-area__sidebar wow fadeInUp">
                         <h4 className="descender">Steps For Usage!</h4>
                         <div className="group">
-                          <p className="secondary">
-                            <i className="bi bi-telephone-fill"></i> Emergency
-                            Request
-                          </p>
-                          <p className="secondary">
-                            <i className="bi bi-geo-alt-fill"></i> Find Donor
-                          </p>
-                          <p className="secondary">
-                            <i className="bi bi-clock-fill"></i> Donate Blood
-                          </p>
+                          <ul className="nav nav-tabs howTo">
+                            <li className="active">
+                              <a data-toggle="tab" href="#login-form"><i class="fa fa-user-md"></i> Emergency Request
+                              </a>
+                            </li>
+                            <li>
+                              <a data-toggle="tab" href="#registration-form"><i class="fa fa-search"></i> Find Donor
+                              </a>
+                            </li>
+                            <li>
+                              <a data-toggle="tab" href="#donate-form"><i class='fas fa-hand-holding-heart'></i> Donate Blood
+                              </a>
+                            </li>
+                          </ul>
                         </div>
                       </div>
                     </div>
+
                     <div className="col-lg-7 row-item">
                       <div className="contact-area-single contact-area__content-form">
-                        <h4 className="descender">Get In Touch</h4>
-                        <p>
-                          On the other hand, we denounce with righteous
-                          indignation and dislike men who are so beguiled and
-                          righteous indignation and dislike men by the charms
-                        </p>
-                        <form action="#" method="post" name="contact__Form">
-                          <div className="input-group-column">
-                            <div className="input">
-                              <input
-                                type="text"
-                                name="contact_f_name"
-                                id="contactFName"
-                                placeholder="First Name"
-                                required
-                                className="input"
-                              />
+
+                        <div className="body">
+                          <div className="tab-content">
+                            <div id="login-form" className="tab-pane fade in active">
+                              <h4 className="descender">Emergency Request</h4>
+                              <p>
+                                On the other hand, we denounce with righteous
+                                indignation and dislike men who are so beguiled and
+                                righteous indignation and dislike men by the charms
+                              </p>
+                              <form action="/">
+
+                                <div className="input-group-column">
+                                  <div className="input">
+                                    <input
+                                      type="text"
+                                      name="contact_f_name"
+                                      id="contactFName"
+                                      placeholder="First Name"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                  <div className="input">
+                                    <input
+                                      type="text"
+                                      name="contact_l_name"
+                                      id="contactLName"
+                                      placeholder="Last Name"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                </div>
+                                <div className="input-group-column">
+                                  <div className="input">
+                                    <input
+                                      type="email"
+                                      name="contact_mail"
+                                      id="contactMail"
+                                      placeholder="Email"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                  <div className="input">
+                                    <input
+                                      type="text"
+                                      name="contact_subject"
+                                      id="contactSubject"
+                                      placeholder="Subject"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                </div>
+                                <div className="input">
+                                  <textarea
+                                    name="contact_message"
+                                    id="contactMessage"
+                                    cols="30"
+                                    rows="10"
+                                    className="input textarea"
+                                    placeholder="Message"
+                                  ></textarea>
+                                </div>
+                                <button
+                                  type="submit"
+                                  className="button button--effect howTo"
+                                >
+                                  Submit Request<i className="bi bi-arrow-right"></i>
+                                </button>
+                              </form>
                             </div>
-                            <div className="input">
-                              <input
-                                type="text"
-                                name="contact_l_name"
-                                id="contactLName"
-                                placeholder="Last Name"
-                                required
-                                className="input"
-                              />
+                            <div id="registration-form" className="tab-pane fade">
+                              <h4 className="descender">Find Donor</h4>
+                              <p>
+                                On the other hand, we denounce with righteous
+                                indignation and dislike men who are so beguiled and
+                                righteous indignation and dislike men by the charms
+                              </p>
+                              <form action="/">
+                                <div className="input-group-column">
+                                  <div className="input">
+                                    <input
+                                      type="text"
+                                      name="contact_f_name"
+                                      id="contactFName"
+                                      placeholder="First Name"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                  <div className="input">
+                                    <input
+                                      type="text"
+                                      name="contact_l_name"
+                                      id="contactLName"
+                                      placeholder="Last Name"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                </div>
+                                <div className="input-group-column">
+                                  <div className="input">
+                                    <input
+                                      type="email"
+                                      name="contact_mail"
+                                      id="contactMail"
+                                      placeholder="Email"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                  <div className="input">
+                                    <input
+                                      type="text"
+                                      name="contact_subject"
+                                      id="contactSubject"
+                                      placeholder="Subject"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                </div>
+                                <div className="input">
+                                  <textarea
+                                    name="contact_message"
+                                    id="contactMessage"
+                                    cols="30"
+                                    rows="10"
+                                    className="input textarea"
+                                    placeholder="Message"
+                                  ></textarea>
+                                </div>
+                                <button
+                                  type="submit"
+                                  className="button button--effect howTo"
+                                >
+                                  Submit Request<i className="bi bi-arrow-right"></i>
+                                </button>
+                              </form>
+                            </div>
+
+
+                            <div id="donate-form" className="tab-pane fade">
+                              <h4 className="descender">Donate Blood</h4>
+                              <p>
+                                On the other hand, we denounce with righteous
+                                indignation and dislike men who are so beguiled and
+                                righteous indignation and dislike men by the charms
+                              </p>
+                              <form action="/">
+                                <div className="input-group-column">
+                                  <div className="input">
+                                    <input
+                                      type="text"
+                                      name="contact_f_name"
+                                      id="contactFName"
+                                      placeholder="First Name"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                  <div className="input">
+                                    <input
+                                      type="text"
+                                      name="contact_l_name"
+                                      id="contactLName"
+                                      placeholder="Last Name"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                </div>
+                                <div className="input-group-column">
+                                  <div className="input">
+                                    <input
+                                      type="email"
+                                      name="contact_mail"
+                                      id="contactMail"
+                                      placeholder="Email"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                  <div className="input">
+                                    <input
+                                      type="text"
+                                      name="contact_subject"
+                                      id="contactSubject"
+                                      placeholder="Subject"
+                                      required
+                                      className="input"
+                                    />
+                                  </div>
+                                </div>
+                                <div className="input">
+                                  <textarea
+                                    name="contact_message"
+                                    id="contactMessage"
+                                    cols="30"
+                                    rows="10"
+                                    className="input textarea"
+                                    placeholder="Message"
+                                  ></textarea>
+                                </div>
+                                <button
+                                  type="submit"
+                                  className="button button--effect howTo"
+                                >
+                                  Submit Request<i className="bi bi-arrow-right"></i>
+                                </button>
+                              </form>
                             </div>
                           </div>
-                          <div className="input-group-column">
-                            <div className="input">
-                              <input
-                                type="email"
-                                name="contact_mail"
-                                id="contactMail"
-                                placeholder="Email"
-                                required
-                                className="input"
-                              />
-                            </div>
-                            <div className="input">
-                              <input
-                                type="text"
-                                name="contact_subject"
-                                id="contactSubject"
-                                placeholder="Subject"
-                                required
-                                className="input"
-                              />
-                            </div>
-                          </div>
-                          <div className="input">
-                            <textarea
-                              name="contact_message"
-                              id="contactMessage"
-                              cols="30"
-                              rows="10"
-                              className="input textarea"
-                              placeholder="Message"
-                            ></textarea>
-                          </div>
-                          <button
-                            type="submit"
-                            className="button button--effect"
-                          >
-                            Submit Request<i className="bi bi-arrow-right"></i>
-                          </button>
-                        </form>
+                        </div>
+
                       </div>
                     </div>
+
                   </div>
                 </div>
               </div>
