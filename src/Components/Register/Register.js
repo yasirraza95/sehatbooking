0731b95a49312ec409479bdfcae3a207939a139e
Yasir Raza <raza.yasir95@gmail.com>
2023-05-15 22:58:43 +0500

    suggestt

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -29,6 +29,7 @@ export default function Register() {
   // useImportScript("/assets/js/main.js");
   const [state, setState] = useState([]);
   const [city, setCity] = useState([]);
+  const [area, setArea] = useState([]);
   const [group, setGroup] = useState([]);
 
   const [notified, setNotified] = useState(false);
@@ -37,10 +38,31 @@ export default function Register() {
   const [submitMessage, setSubmitMessage] = useState("");
   const [emailError, setEmailError] = useState("");
   const [phoneError, setPhoneError] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
 
-  const onChange = (value) => {
-    console.log("Captcha value:", value);
-  }
+  const fetchAreas = (e) => {
+    const getCityArea = async () => {
+      setArea([]);
+      const { data } = await GeneralService.getCityArea(selectedCity, e);
+      const { response: res } = data;
+      const results = [];
+      res.map((value) => {
+        results.push({
+          key: value.name,
+          value: value.name,
+        });
+      });
+      setArea([...results]);
+    };
+
+    if (e.target.value !== "") {
+      getCityArea();
+    }
+  };
+
+  const changeCity = (e) => {
+    setSelectedCity(e);
+  };
 
   const changeState = (e) => {
     const getCity = async () => {
@@ -277,12 +299,12 @@ export default function Register() {
                       </div>
                     </div>
                     <div className="input-group-column">
-
                       <div className="input">
                         <label for="regiState">State</label>
                         <select
                           // className="selectpicker"
-                          className="form-select" aria-label="Default select example"
+                          className="form-select"
+                          aria-label="Default select example"
                           name="state"
                           id="regstate"
                           value={values.state || ""}
@@ -307,12 +329,13 @@ export default function Register() {
                         <label for="regiCity">City</label>
                         <select
                           // className="selectpicker"
-                          className="form-select" aria-label="Default select example"
+                          className="form-select"
+                          aria-label="Default select example"
                           name="city"
                           id="cityReg"
                           value={values.city || ""}
                           onChange={(e) => {
-                            // changeCity(e);
+                            changeCity(e.target.value);
                             handleChange(e);
                           }}
                         >
@@ -331,45 +354,44 @@ export default function Register() {
                       </div>
                     </div>
                     <div className="input-group-column">
-
                       <div className="input">
                         <label for="regicityArea">City Area</label>
                         <input
-                          type="text" list="cityArea"
+                          type="text"
+                          list="cityArea"
                           name="city_area"
                           id="regicityArea"
                           required
                           value={values.city_area || ""}
                           onChange={handleChange}
                         />
-                        <datalist id="cityArea">
-                          <option value="Objective C">Objective C</option>
-                          <option value="C++">C++</option>
-                          <option value="C#">C#</option>
-                          <option value="Cobol">Cobol</option>
-                          <option value="Go">Go</option>
-                          <option value="Java">Java</option>
-                          <option value="JavaScript">JavaScript</option>
-                          <option value="Python">Python</option>
-                          <option value="PHP">PHP</option>
-                          <option value="Pascal">Pascal</option>
-                          <option value="Perl">Perl</option>
-                          <option value="R">R</option>
-                          <option value="Swift">Swift</option>
+                        <datalist
+                          id="cityArea"
+                          onChange={(e) => {
+                            fetchAreas(e.target.value);
+                            handleChange(e);
+                          }}
+                        >
+                          {/* <option value="Objective C">Objective C</option> */}
+                          {area.map((res) => {
+                            return (
+                              <option key={res.value} value={res.value}>
+                                {res.value}
+                              </option>
+                            );
+                          })}
                         </datalist>
                         {touched.city_area && errors.city_area && (
-                          <div className="errorArea">
-                            {errors.city_area}
-                          </div>
+                          <div className="errorArea">{errors.city_area}</div>
                         )}
                       </div>
-
 
                       <div className="input">
                         <label for="regiCountry">Blood Group</label>
                         <select
                           // className="selectpicker"
-                          className="form-select" aria-label="Default select example"
+                          className="form-select"
+                          aria-label="Default select example"
                           id="regiCountry"
                           name="group"
                           onChange={handleChange}
@@ -390,13 +412,12 @@ export default function Register() {
                     </div>
 
                     <div className="input-group-column">
-
                       <div className="input">
                         <label for="regiState">Date Of Birth</label>
                         <input
                           type="date"
                           name="dob"
-                          id="regiState"
+                          id="dob"
                           required
                           value={values.dob || ""}
                           onChange={handleChange}
@@ -411,15 +432,13 @@ export default function Register() {
                         <input
                           type="date"
                           name="last_bleed"
-                          id="regiState"
+                          id="last_bleed"
                           required
                           value={values.last_bleed || ""}
                           onChange={handleChange}
                         />
                         {touched.last_bleed && errors.last_bleed && (
-                          <div className="error">
-                            {errors.last_bleed}
-                          </div>
+                          <div className="error">{errors.last_bleed}</div>
                         )}
                       </div>
                     </div>
