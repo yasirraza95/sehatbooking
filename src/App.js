30c5416b25afe3b9560638d21191ceba47a8ee0e
masood911 <masoodjutt64@gmail.com>
2023-05-21 11:19:41 -0700

    new development and code changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { FloatingWhatsApp } from "react-floating-whatsapp";
 
 import "./App.css";
@@ -24,37 +24,41 @@ import ForgotToken from "./Components/ForgotToken/ForgotToken";
 import PreLoader from "./Components/PreLoader/PreLoader";
 import HomeScreen from "./Components/Home/HomeScreen";
 import NoInternet from "./Components/NoInternet/NoInternet";
+import PageNotFound from "./Components/PageNotFound/PageNotFound";
 
 function App() {
   return (
     <>
       <div className="App">
         <NoInternet>
-        <BrowserRouter>
-          <PreLoader />
-          <Routes>
-            <Route path="/" element={<IndexOne />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/index-one" element={<IndexOne />} />
-            <Route path="/faqs" element={<Faqs />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-conditions" element={<TermsConditions />} />
-            <Route path="/about-us" element={<AboutUs />} />
-            <Route path="/forgot-pass" element={<ForgotPass />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/contact-us" element={<ContactUs />} />
-            {/* <Route path="/campaign" element={<Campaign />} /> */}
-            <Route path="/blood-tips" element={<BloodTips />} />
-            <Route path="/find-donor" element={<FindDonor />} />
-            <Route path="/appointment" element={<Appointment />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/how-to" element={<HowTo />} />
-            <Route path="/our-services" element={<OurServices />} />
-            <Route path="/service-detail" element={<ServiceDetail />} />
-            <Route path="/blood-request" element={<BloodRequest />} />
-            <Route path="/forgot-token" element={<ForgotToken />} />
-          </Routes>
-        </BrowserRouter>
+          <BrowserRouter>
+            <PreLoader />
+            <Routes>
+              <Route path="/" element={<IndexOne />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/index-one" element={<IndexOne />} />
+              <Route path="/faqs" element={<Faqs />} />
+              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="/terms-conditions" element={<TermsConditions />} />
+              <Route path="/about-us" element={<AboutUs />} />
+              <Route path="/forgot-pass" element={<ForgotPass />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/contact-us" element={<ContactUs />} />
+              {/* <Route path="/campaign" element={<Campaign />} /> */}
+              <Route path="/blood-tips" element={<BloodTips />} />
+              <Route path="/find-donor" element={<FindDonor />} />
+              <Route path="/appointment" element={<Appointment />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/how-to" element={<HowTo />} />
+              <Route path="/our-services" element={<OurServices />} />
+              <Route path="/service-detail" element={<ServiceDetail />} />
+              <Route path="/blood-request" element={<BloodRequest />} />
+              <Route path="/forgot-token" element={<ForgotToken />} />
+              <Route path="*" element={<PageNotFound />} />
+            {/* <Route path="*" element={<Navigate to="/" />} /> */}
+            </Routes>
+
+          </BrowserRouter>
         </NoInternet>
       </div>
       <FloatingWhatsApp
---
+++ src/Components/PageNotFound/PageNotFound.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./PageNotFound.css";
+import TopBar from '../HomeTopBar/TopBar';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+export default function PageNotFound() {
+    return (
+        <>
+            <TopBar />
+            <Header />
+            <div id="notfound">
+                <div className="notfound-bg"></div>
+                <div className="notfound">
+                    <div className="notfound-404">
+                        <h1>404</h1>
+                    </div>
+                    <h2>we are sorry, but the page you requested was not found</h2>
+                    <a href="#" className="home-btn">Go Home</a>
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
